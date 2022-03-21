2bf05923043c456074d8a6b357585b04efc9d1e5
Saurabh Kumar <saurabhkr9902@gmail.com>
2022-03-21 18:02:30 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Tabs from "./component/TabMenu/Tabs";
+import GalleryReact from "./component/TabMenu/GalleryReact";
+
+const App = () => {
+  return (
+    <>
+     <GalleryReact />
+    </>
+  )
+}
+
+export default App
